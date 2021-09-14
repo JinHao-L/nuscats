feadd86bacd8369144c1c979faf8bb486b70a40c
Jin Hao <limjinhao@gmail.com>
2021-09-14 17:27:12 +0800

    Remove backend import dependency on frontend

--- frontend/src/pages/CatsTab.tsx
+++ frontend/src/pages/CatsTab.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import CatCard from 'components/CatCard';
-import { Cat, UniversityZone } from '../../../backend/src/cats/cat.entity'
+import { Cat, UniversityZone } from 'types/cat';
 
 const CatsTab: React.FC = () => {
   // Mock data
