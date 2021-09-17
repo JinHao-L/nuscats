31a6ec10d49dbd56c581756f054c98edf0bd7927
dominiclet <dominic.let@hotmail.com>
2021-09-17 12:44:32 +0800

    Add bg image to landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -4,20 +4,24 @@ const Landing: React.FC = () => {
 	return (
 		<IonPage>
 			<IonContent>
-				<div className="flex flex-col-reverse h-full mx-5 pb-20">
-					<p className="text-sm text-center mb-6">
-						Already a member? <IonRouterLink routerLink="/signin" routerDirection="forward">Sign in</IonRouterLink>
-					</p>
-					<IonButton
-						className="mx-5 h-14 mb-6 cursor-pointer font-bold"
-						color="primary"
-						expand="block"
-						routerLink="/signup"
-						routerDirection="forward"
-					>
-						Join our community
-					</IonButton>
-					<p className="text-3xl mb-6 font-bold text-center">Insert tagline here</p>
+				<div className="h-full bg-cover-cat">
+					<div className="h-full bg-gradient-to-b from-transparent to-black">
+						<div className="flex flex-col-reverse h-full mx-5 pb-20">
+							<p className="text-sm text-center mb-6 text-white">
+								Already a member? <IonRouterLink routerLink="/signin" routerDirection="forward">Sign in</IonRouterLink>
+							</p>
+							<IonButton
+								className="mx-5 h-14 mb-6 cursor-pointer font-bold"
+								color="primary"
+								expand="block"
+								routerLink="/signup"
+								routerDirection="forward"
+								>
+								Join our community
+							</IonButton>
+							<p className="text-3xl mb-6 font-bold text-center text-white">Insert tagline here</p>
+						</div>
+					</div>
 				</div>
 			</IonContent>
 		</IonPage>
