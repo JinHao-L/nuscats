d60cb533f4948661652b477ebd4b28c8e9ad2709
Jin Hao <limjinhao@gmail.com>
2021-09-21 01:47:24 +0800

    Add profile pic to seeded profiles

--- backend/src/seeders/seeds/users.seeder.ts
+++ backend/src/seeders/seeds/users.seeder.ts
@@ -57,6 +57,8 @@ export class UsersSeeder implements ISeeder {
             user: user,
             first_name: user.username,
             last_name: user.username,
+            profile_pic:
+              Math.random() < 0.5 ? 'https://i.pravatar.cc/300' : null,
           }),
         );
 
