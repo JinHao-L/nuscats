8c19d14463fe2561fd99050ce047e43a46c2952b
dominiclet <dominic.let@hotmail.com>
2021-09-20 21:45:00 +0800

    Link edit cats with backend

--- frontend/src/pages/EditCatsList.tsx
+++ frontend/src/pages/EditCatsList.tsx
@@ -2,39 +2,37 @@ import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonPage, I
 import { Cat, makeCat, UniversityZone } from '@api/cats';
 import AdminCatCard from "components/AdminCatCard";
 import EditCatModal from "components/EditCatModal";
+import { useCats } from "hooks/useCats";
 
 const EditCatsList: React.FC =() => {
-	// Dummy data
-	const cats: Cat[] = [
-		makeCat({
-		id: 1,
-		name: "Garfield",
-		neutered: false,
-		one_liner: "Fluffy bowling ball",
-		description: "I am a strong and healthy boy! I have black and white fur and I love to sleep :)",
-		zone: UniversityZone.Arts
-		}),
-		makeCat({
-		id: 2,
-		name: "Ashy",
-		neutered: false,
-		one_liner: "Bachelor's in Laziness, Masters's in Belly Flops and PhD in Napping",
-		description: "I sleep most of the day for a very good excuse- that’s what I’m wired to do! When most of you are just waking up, or rushing for that 8am tutorial, I am starting the day with a snooze. You might pass me by on the way to the UTown bus stop, at the steps outside UTR. You can probably approach me quietly for a little pat and rub, but please don’t poke me awake – I need my catnaps.",
-		zone: UniversityZone.Utown
-		}),
-		makeCat({
-		id: 3,
-		name: "Ashy",
-		description: "I sleep most of the day for a very good excuse- that’s what I’m wired to do! When most of you are just waking up, or rushing for that 8am tutorial, I am starting the day with a snooze. You might pass me by on the way to the UTown bus stop, at the steps outside UTR. You can probably approach me quietly for a little pat and rub, but please don’t poke me awake – I need my catnaps.",
-		zone: UniversityZone.Utown
-		}),
-		makeCat({
-		id: 4,
-		name: "Ashy",
-		description: "I sleep most of the day for a very good excuse- that’s what I’m wired to do! When most of you are just waking up, or rushing for that 8am tutorial, I am starting the day with a snooze. You might pass me by on the way to the UTown bus stop, at the steps outside UTR. You can probably approach me quietly for a little pat and rub, but please don’t poke me awake – I need my catnaps.",
-		zone: UniversityZone.Utown
-		})
-	]
+	const { cats, error, isLoading, mutate } = useCats();
+	let pageContent;
+	if (isLoading) {
+		pageContent = (
+			<IonContent>
+				Loading...
+			</IonContent>
+		);
+	} else {
+		const catsData = cats as Cat[];
+		pageContent = (
+			<IonContent>
+				<div className="flex flex-col mx-5 my-5">
+					{catsData.map(cat =>
+						<AdminCatCard key={cat.id} cat={cat} cardColor="bg-tertiary-300" editCard />
+					)}
+				</div>
+				<IonButton
+					className="mx-5 text-lg text-white cursor-pointer h-14"
+					color="primary"
+					expand="block"
+					onClick={() => presentModal()}
+				>
+					Add cat 
+				</IonButton>
+			</IonContent>
+		);
+	}
 
 	// Add new cat modal
 	const [presentModal, dismissModal] = useIonModal(EditCatModal, {
@@ -53,21 +51,7 @@ const EditCatsList: React.FC =() => {
 					</IonTitle>
 				</IonToolbar>
 			</IonHeader>
-			<IonContent>
-				<div className="flex flex-col mx-5 my-5">
-					{cats.map(cat =>
-						<AdminCatCard key={cat.id} cat={cat} cardColor="bg-tertiary-300" editCard />
-					)}
-				</div>
-				<IonButton
-					className="mx-5 text-lg text-white cursor-pointer h-14"
-					color="primary"
-					expand="block"
-					onClick={() => presentModal()}
-				>
-					Add cat 
-				</IonButton>
-			</IonContent>
+			{pageContent}
 		</IonPage>
 	);
 }
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,14 +1,14 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonRouterLink, IonTitle, IonToolbar } from "@ionic/react";
 import { settingsOutline } from "ionicons/icons";
 import { ImageDetail, ImageGallery } from "components/ImageGallery";
-import { PROFILE_SETTINGS_ROUTE } from "app/routes";
+import { PROFILE_SETTINGS_ROUTE, SIGNIN_ROUTE } from "app/routes";
+import useAuth from "hooks/useAuth";
+import { useEffect } from "react";
 
 
 const Profile: React.FC = () => {
 	// Dummy data
 	const profilePicUrl = "https://placekitten.com/80/80";
-	const username = "Username here";
-	const fullname = "Full name here";
 	const sightings = 300;
     const placeholderCatImgGalleryDetails: ImageDetail[] =
         Array.from({ length: 16 })
@@ -17,10 +17,37 @@ const Profile: React.FC = () => {
                 src: `http://placekitten.com/${400 + idx}/${300 + idx}`
             }))
 
+	// Fetch profile data
+  	const { isLoggedIn, userId, userProfile, setLogin, setLogout } = useAuth();
+
+	// Page to display if user is not signed in
+	if (!isLoggedIn) {
+		return (
+			<IonPage>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>
+							Profile
+						</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonContent>
+					<div className="flex flex-col justify-center h-full">
+						<p className="text-xl font-semibold text-center text-secondary-500">
+							<IonRouterLink routerLink={SIGNIN_ROUTE}>Sign in</IonRouterLink> to view profile
+						</p>
+					</div>
+				</IonContent>
+			</IonPage>
+		);
+	}
+
+	const username = userProfile?.user?.username;
+	const fullname = userProfile?.first_name as string + " " + userProfile?.last_name as string;
+
 	return(
 		<IonPage>
-			<IonHeader collapse="condense">
-				<IonToolbar />
+			<IonHeader>
 				<IonToolbar>
 					<IonButtons slot="end">
 						<IonButton
@@ -39,17 +66,16 @@ const Profile: React.FC = () => {
 			</IonHeader>
 			<IonContent>
 				<div className="flex flex-col h-full mx-5">
-					
-					<div className="p-5 mt-2 border bg-primary-400 rounded-3xl">
+					<div className="p-5 mt-2 border shadow-xl bg-secondary-200 rounded-3xl">
 						<div className="flex">
 							<img src={profilePicUrl} alt="profile pic" className="w-20 h-20 border-2 border-white rounded-3xl" />
 							<div className="w-full ml-3">
-								<p className="text-xl font-bold text-white">{username}</p>
-								<p className="text-white">{fullname}</p>
+								<p className="text-xl font-bold">{username}</p>
+								<p>{fullname}</p>
 							</div>
 						</div>
 						<div className="flex justify-center mt-5">
-							<div className="text-center text-tertiary-200">
+							<div className="text-center text-tertiary-500">
 								<p className="text-lg font-semibold">{sightings}</p>
 								<p className="text-sm">sightings</p>
 							</div>
